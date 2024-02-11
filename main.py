d1ba8b5a12018c10a902dc80fdc7514f6a49b963
Chris <86209684+Glorentino@users.noreply.github.com>
2024-02-11 02:20:57 -0500

    Update main.py
    Added the delete feature

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ class AppleMusic:
         self.songsLibrary = [] # show songs
         self.artistLibrary = {} # shows artist
         
-    def store(self, inputInfo):
-        songName, artist, albumName, genre = inputInfo
+    def store(self, storeInfo):
+        songName, artist, albumName, genre = storeInfo
         if albumName not in self.library:
             self.library[albumName] = []
             self.library[albumName].append(songName)
@@ -34,13 +34,36 @@ class AppleMusic:
                 self.artistLibrary[artist][albumName].append(songName)
         return "Added to Library"
     
-    def delete(self):
-        pass
+    def delete(self, deleteInfo):
+        typeInfo, songName, artist, albumName, genre = deleteInfo
+        if typeInfo == "song":
+            self.songsLibrary.remove(songName+"-"+artist)
+            self.library[albumName].remove(songName)
+            self.artistLibrary[artist][albumName].remove(songName)
+        if typeInfo == "artist":
+            for album in self.artistLibrary[artist]:
+                for song in self.artistLibrary[artist][album]:
+                    self.songsLibrary.remove(song+"-"+artist)
+                del self.library[album] 
+            del self.artistLibrary[artist]
+        if typeInfo == "album":
+            for song in self.artistLibrary[artist][albumName]:
+                self.songsLibrary.remove(song+"-"+artist)
+            del self.artistLibrary[artist][albumName]
+            del self.library[albumName]
+        print(self.library)
+        
+        return "Removed from library"
+    
     def display(self):
         return self.artistLibrary, self.library, self.songsLibrary
 
 appleMusic = AppleMusic()
 print(appleMusic.store(["Glorious Day", "Glory Baby", "Glory Days", "Soul"]))
 print(appleMusic.store(["Glorious Day 2", "Glory Baby", "Glory Days", "Soul"]))
-print(appleMusic.store(["Glorious Day 3", "Glory Baby", "Glory Days", "Soul"]))
+print(appleMusic.store(["Glorious Day 2", "Glory Baby", "Glory Days", "Soul"]))
+print()
+print(appleMusic.store(["Half moon Japanese", "Carmello Vision", "7", "R&B"]))
+print(appleMusic.store(["Aisheteru", "Shota-Chwan", "Shota ASSaSIN", "Hip Hop"]))
 print(appleMusic.display())
+print(appleMusic.delete(["album", "Aisheteru", "Shota-Chwan", "Shota ASSaSIN", "Hip Hop"]))
